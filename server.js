aba0abcefb495e83301f51b38b02179867ff255c
CriptoJay <91977435+CRIPTOJAY09@users.noreply.github.com>
2025-07-03 12:27:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Configuración mejorada
 const CAPITAL = 50000;
